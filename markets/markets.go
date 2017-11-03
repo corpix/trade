cfae4edce7307a9420724b3d527bf27d44adbc2a
Dmitry Moskowski <me@corpix.ru>
2017-11-03 18:17:14 +0000

    Fixed tests

--- markets/markets.go
+++ markets/markets.go
@@ -1,8 +1,9 @@
 package markets
 
 import (
-	"net/http"
 	"strings"
+
+	"github.com/corpix/loggers"
 
 	"github.com/cryptounicorns/trade/markets/market"
 	"github.com/cryptounicorns/trade/markets/market/bitfinex"
@@ -12,11 +13,13 @@ const (
 	BitfinexMarket = bitfinex.Name
 )
 
-func New(market string) (market.Market, error) {
+func New(market string, config Config, logger loggers.Logger) (market.Market, error) {
 	switch strings.ToLower(market) {
 	case BitfinexMarket:
-		// FIXME: config + logger for constructor
-		return bitfinex.New()
+		return bitfinex.New(
+			config.Bitfinex,
+			logger,
+		), nil
 	default:
 		return nil, NewErrUnsupportedMarket(market)
 	}
