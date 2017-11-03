6a090ab6445d7af90489d997a3076f3eee518f20
Dmitry Moskowski <me@corpix.ru>
2017-11-03 17:54:49 +0000

    Get rid of needless fields in ticker
    This fields was here because initialy ticker was designed
    around wex.nz API

--- markets/market/ticker.go
+++ markets/market/ticker.go
@@ -4,16 +4,15 @@ import (
 	"github.com/cryptounicorns/trade/currencies"
 )
 
+// Timestamp MUST be always UTC UnixNano timestamp
 type Ticker struct {
 	High         float64                 `json:"high"`
 	Low          float64                 `json:"low"`
-	Avg          float64                 `json:"avg"`
 	Vol          float64                 `json:"vol"`
-	VolCur       float64                 `json:"volCur"`
 	Last         float64                 `json:"last"`
 	Buy          float64                 `json:"buy"`
 	Sell         float64                 `json:"sell"`
-	Timestamp    float64                 `json:"timestamp"`
+	Timestamp    uint64                  `json:"timestamp"`
 	CurrencyPair currencies.CurrencyPair `json:"currencyPair"`
 	Market       string                  `json:"market"`
 }
