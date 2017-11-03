aef658585ec6fbf4f1870ec24ef4e8a87beddfc6
Dmitry Moskowski <me@corpix.ru>
2017-11-03 23:42:39 +0000

    Move fiat currencies out of generator

--- generators.mk
+++ generators.mk
@@ -2,7 +2,18 @@ tools := ./tools
 
 .PHONY: currencies/currencies.go
 currencies/currencies.go:
-	go run    $(tools)/coinmarketcap/coinmarketcap.go all \
-		| $(tools)/generate-currencies --verbose      \
-		> $@
+	# FIXME: Fiat currencies are also here, but they should be delivered from some resource
+	{                                                                                    \
+		set -e;                                                                      \
+		echo '{"name": "China Yan",            "symbol": "CNY", "volume": 9999999}'; \
+		echo '{"name": "Japanese Yen",         "symbol": "JPY", "volume": 9999999}'; \
+		echo '{"name": "Russian Ruble",        "symbol": "RUB", "volume": 9999999}'; \
+		echo '{"name": "United States Dollar", "symbol": "USD", "volume": 9999999}'; \
+		echo '{"name": "Euro",                 "symbol": "EUR", "volume": 9999999}'; \
+		echo '{"name": "Canadian Dollar",      "symbol": "CAD", "volume": 9999999}'; \
+		go run $(tools)/coinmarketcap/coinmarketcap.go all;                          \
+	} | $(tools)/generate-currencies > $@
 	go fmt $@
+
+.PHONY: generate
+generate:: currencies/currencies.go
