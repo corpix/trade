5c12b7bcf7e36a49c2ef210b1b22897f40377e39
Dmitry Moskowski <me@corpix.ru>
2017-11-29 20:32:27 +0000

    Fixing currency pairs

--- generators.mk
+++ generators.mk
@@ -9,7 +9,7 @@ markets_currencies := $(foreach market,$(markets),$(markets_root)/$(market)/curr
 
 .PHONY: $(all_currencies)
 $(all_currencies):
-	# FIXME: Fiat should be automatically downloaded for each market!
+	# XXX: Not all fiat currencies could be traded on all markets, you should check by hands!
 	{                                                                                    \
 		set -e;                                                                      \
 		echo '{"name": "china-yan",            "symbol": "CNY", "volume": 9999999}'; \
@@ -23,10 +23,15 @@ $(all_currencies):
 
 .PHONY: $(markets_currencies)
 $(markets_currencies):
-	# FIXME: Fiat should be automatically downloaded for each market!
+	# XXX: Not all fiat currencies could be traded on all markets, you should check by hands!
 	{                                                                                    \
 		set -e;                                                                      \
+		echo '{"name": "china-yan",            "symbol": "CNY", "volume": 9999999}'; \
+		echo '{"name": "japanese-yen",         "symbol": "JPY", "volume": 9999999}'; \
+		echo '{"name": "russian-ruble",        "symbol": "RUB", "volume": 9999999}'; \
 		echo '{"name": "united-states-dollar", "symbol": "USD", "volume": 9999999}'; \
+		echo '{"name": "euro",                 "symbol": "EUR", "volume": 9999999}'; \
+		echo '{"name": "canadian-dollar",      "symbol": "CAD", "volume": 9999999}'; \
 		go run $(tools_root)/coinmarketcap/coinmarketcap.go                          \
 			exchanges --exchange=$(shell basename $(@:%/currencies.json=%));     \
 	} | $(tools_root)/postprocess-currencies --verbose | jq . > $@
